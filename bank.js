b6b63fc014f36385d5c4663eda6d08ae289343b9
David MacKintosh <dgawmackintosh@gmail.com>
2016-06-03 12:58:07 +0100

    Adds find largest account function

--- bank.js
+++ bank.js
@@ -11,11 +11,30 @@ Bank.prototype = {
     this.accounts.push( account )
   },
 
-  searchByName: function( accountName ) {
-    var result = _.find( this.accounts, function(account) { 
+  searchByName: function( accountName ){
+    var result = _.find( this.accounts, function(account ) { 
       return account.name === accountName; 
     });
     return result;
+  },
+
+  // filteredAccounts: function( type ){
+    
+  // },
+
+  findLargestAccount: function(){
+    var largest = this.accounts[0];
+    this.accounts.forEach( function( account ){
+      if( account.value > largest.value ){
+        largest = account
+      }
+    } )
+      return largest;
+  },
+
+  findCombinedValue: function(){
+    var total = 0;
+    
   }
 
 };
--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -5,21 +5,35 @@ var assert = require( 'chai' ).assert
 
 describe( "Bank", function(){
 
-  var royalbank = new Bank( "Royal Bank" );
+  var codeBank = new Bank( "Code Bank" );
 
   var davidAccount = new Account( "David", "personal", 500 )
+  var alistairAccount = new Account( "Alistair", "personal", 750 )
+  var codeClanAccount = new Account( "Code Clan", "business", 10000 )
+
 
   it( "Starts with empty array of accounts", function(){
-    assert.deepEqual( [], royalbank.accounts )
+    assert.deepEqual( [], codeBank.accounts )
   } );
 
   it( "Can add an account", function(){
-    royalbank.addAccount( davidAccount )
-    assert.deepEqual( [davidAccount], royalbank.accounts )
+    codeBank.addAccount( davidAccount )
+    assert.deepEqual( [davidAccount], codeBank.accounts )
   } );
 
   it( "Can find account by owner name", function(){
-    assert.deepEqual( davidAccount, royalbank.searchByName( "David" ) )
+    assert.deepEqual( davidAccount, codeBank.searchByName( "David" ) )
+  } );
+
+  it( "Can find largest account", function(){
+    codeBank.addAccount( alistairAccount )
+    codeBank.addAccount( codeClanAccount )
+
+    assert.deepEqual( codeClanAccount, codeBank.findLargestAccount() )
+  } );
+
+  it( "Can find combined account value", function(){
+    assert.deepEqual( 11250, codeBank.findCombinedValue() )
   } );
 
 } )
